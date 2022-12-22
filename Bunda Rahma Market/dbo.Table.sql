ed4c9122ad51623ad43343f62b8e7fc263d3b116
Hefaistx <nabililyasa2002@gmail.com>
2022-12-22 16:44:20 +0700

    + class DB

---
+++ Bunda Rahma Market/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+    [prodId] INT NOT NULL, 
+    [prodName] NCHAR(20) NULL, 
+    [prodPrice] INT NULL, 
+    [prodQty] INT NULL, 
+    [prodCat] NCHAR(30) NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([prodId])
+)
